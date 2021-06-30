1fa64dc59449bab9473107448467b077123b1814
Joao Vitor Vieira Claro <joao.claro@arcomsa.ad>
2021-06-30 17:44:36 -0300

    first commit

--- API/Admin/rotas.py
+++ API/Admin/rotas.py
@@ -1,41 +1,50 @@
 from flask import *
 from API import app, db
 from .models import INSS, IRFF
-from sqlalchemy import between
+import json
 
-
-
-@app.route('/')
+@app.route('/',methods=['GET'])
 def home():
     return render_template('admin/index.html')
 
-@app.route('/salario', methods=['GET','POST'])
+@app.route('/salario', methods=['POST'])
 def salario():
     if(request.method =='POST'):
-        Salario_bruto = float(request.form.get('salario_bruto'))
-        Descontos = float(request.form.get('Descontos'))
-        Dependentes = int(request.form.get('Dependentes'))
+        
+        Salario_bruto = request.json["salario_bruto"]   
+        Descontos =  request.json["Descontos"]
+        Dependentes = request.json["Dependentes"]
+        
+        #body = request.get_json()
+        #Salario_bruto = body["salario_bruto"]       
+        #Descontos = body["Descontos"] 
+        #Dependentes = body["Dependentes"] 
+        
+        #Salario_bruto = float(request.form.get('salario_bruto'))
+        #Descontos = float(request.form.get('Descontos'))
+        #Dependentes = int(request.form.get('Dependentes'))
         
         inss = INSS.query.filter(INSS.faixa_inicial <= Salario_bruto) or (INSS.faixa_final >= Salario_bruto)   
         for ins in inss:
-            aliquota = ins.aliquota
-            deducao = ins.deduçao       
-        reajuste_inss = (Salario_bruto/(100*aliquota))-float(deducao)  
+            aliquota = (ins.aliquota/100)
+            deducao = ins.deduçao
+          
+        reajuste_inss = (Salario_bruto*aliquota)-float(deducao)  
         
         if (Salario_bruto >= 1903.98):  
-            salariobase = Salario_bruto - (Descontos + (Dependentes * 189.59))
+            salariobase = Salario_bruto - (Descontos + (189.59 * Dependentes ))
+            print(salariobase)
             irrf = IRFF.query.filter(IRFF.faixa_inicial <= float(salariobase)) or (IRFF.faixa_final >= float(salariobase))
             for inf in irrf:
-                aliquotairrf = inf.aliquota
-                deducaoirrf = inf.deduçao 
-            
-            reajuste_irrf = (salariobase/(aliquotairrf*100))- float(deducaoirrf)
+                aliquotairrf = (inf.aliquota/100)
+                deducaoirrf = inf.deduçao            
+            reajuste_irrf = (salariobase*aliquotairrf) - float(deducaoirrf)
         else:
-            reajuste_irrf = 0
-        
+            reajuste_irrf = 0   
+                 
         valor_final = reajuste_inss + reajuste_irrf
     
     
-    
-    return render_template('admin/index.html', valor=reajuste_inss , valorirrf=reajuste_irrf, valor_fina=valor_final)
+    return {"valor final": valor_final, "reajuste_inss":reajuste_inss, "reajuste_irrf": reajuste_irrf}
+    #return render_template('admin/index.html', valor=reajuste_inss , valorirrf=reajuste_irrf, valor_fina=valor_final)
     
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from API import app
 
 def main():
     port = int(os.environ.get("PORT",5000))
+
     app.run(host="0.0.0.0", port=port,debug=True)
 
 if __name__ =="__main__":            
