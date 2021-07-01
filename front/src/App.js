71e93a79775c1a78f1801ff4f6ea20ec6d8bafe8
Joao Vitor Vieira Claro <joao.claro@arcomsa.ad>
2021-07-01 17:24:41 -0300

    first commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react'
 import api from './api'
+import "./App.css"
+
+
 
 class App extends Component{
-state = {
-  filmes:[],
-}
-
+  state = {}
+  
+  
 async componentDidMount(){
   const reponse = await api.post('/salario',{
-    salario_bruto:3000,
+    salario_bruto:5000,
     Descontos:0,
     Dependentes:0
 
   });
-  console.log(reponse.data)
-  
-
-  
+  console.log(reponse)
 }
 
   render(){
     
     return(
       <div>
-        <h1>ola mundo</h1>        
+        <div className="div1">
+          <form className="form">
+            <input type="number" step=".01" name="salarioBruto" id="salarioBruto" className="form-item" placeholder="Salario"></input>
+            <input type="number" step=".01" name="desconto"id="desconto"  className="form-item" placeholder="Desconto"></input>
+            <input type="number"  name="dependentes"id="dependentes"  className="form-item" placeholder="Dependente"></input>
+            <button type="submit" className="form-button">Calcular</button>
+          </form>
+        </div>
+        <div className="result">
+                   
+        </div>        
       </div>
-
     );
   }
 }
--- front/src/api.js
+++ front/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://192.168.100.3:5000/',
+    baseURL: 'http://172.19.1.52:5000/',
     timeout:1000
     
     //baseURL: 'https://api.tvmaze.com/search/shows?q='
